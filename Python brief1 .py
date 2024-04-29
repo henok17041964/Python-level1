1e04d21c26ca1dc2775dcf2033ade1f9b3831675
henok17041964 <167239515+henok17041964@users.noreply.github.com>
2024-04-29 03:04:52 +0100

    Add files via upload

---
+++ Python brief1 .py
@@ -0,0 +1,4 @@
+print( "This program will convert your weight from kilogram to pounds.") 
+weight = int ( input( " Enter your weight in KG: "))
+converted = weight / 0.45
+print(f"You are {converted} pounds ")
